f8f1612a2af29fb591c45fe77d268c26062a5971
nadachabir <nadou.chabir@gmail.com>
2026-06-11 10:43:33 +0100

    fix printing text

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,7 +8,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello go")
+	fmt.Println("Hello GO practitioner")
 	fmt.Println("TaskFlow API — Phase 0 scaffold. Build the HTTP server in Phase 1.")
 }
 
